fecc4ead524fa0f6cc964e7e0696b490b997274f
ldhcabadan <zhangyuyi2003@163.com>
2023-10-27 16:13:28 +0800

    proj1 checkpoint 4th try

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -4,7 +4,9 @@ import afu.org.checkerframework.checker.igj.qual.I;
 
 public class ArrayDeque<Item> {
     private Item[] items;
-    private int size;
+    private int length;
+    // length is the maximum length of the array.(Initially, 8)
+    // instead, the size function is the actual size of the array. (Initially, 0)
     private int nextFirst;
     private int nextLast;
 
@@ -13,7 +15,7 @@ public class ArrayDeque<Item> {
      */
     public ArrayDeque() {
         items = (Item[]) new Object[8];
-        size = 8;
+        length = 8;
         nextFirst = 3;
         nextLast = 4;
     }
@@ -28,26 +30,25 @@ public class ArrayDeque<Item> {
 
     private void resizeBig(int capacity) {
         Item[] a = (Item[]) new Object[capacity];
-        System.arraycopy(items, 0, a, size / 2, size);
+        System.arraycopy(items, 0, a, length / 2, length);
         items = a;
-        nextFirst = size / 2 - 1;
-        nextLast = size * 3 / 2;
-        size = capacity;
+        nextFirst = length / 2 - 1;
+        nextLast = length * 3 / 2;
+        length = capacity;
     }
 
     private void resizeSmall(int capacity) {
         Item[] a = (Item[]) new Object[capacity];
-        System.arraycopy(items, nextFirst + 1, a, (size - (nextLast - nextFirst - 1)) / 2, nextLast - nextFirst - 1);
+        System.arraycopy(items, nextFirst + 1, a, (length / 2 - (nextLast - nextFirst - 1)) / 2, size());
         items = a;
         int newNextFirst = 0;
         int newNextLast = 0;
-        newNextFirst = (size - (nextLast - nextFirst - 1)) / 2 - 1;
-        newNextLast = (size - (nextLast - nextFirst - 1)) / 2 + (nextLast - nextFirst);
+        newNextFirst = (length / 2 - (nextLast - nextFirst - 1)) / 2 - 1;
+        newNextLast = (length / 2 - (nextLast - nextFirst - 1)) / 2 + (nextLast - nextFirst) - 1;
         nextFirst = newNextFirst;
         nextLast = newNextLast;
-        size = capacity;
+        length = capacity;
     }
-
 
 
     public void addFirst(Item x) {
@@ -59,10 +60,10 @@ public class ArrayDeque<Item> {
         if (nextFirst != 0) {
             nextFirst--;
         } else {
-            nextFirst = size;
+            nextFirst = length - 1;
         }
         if (resizeFlag) {
-            resizeBig(2 * size);
+            resizeBig(2 * length);
         }
     }
 
@@ -72,58 +73,90 @@ public class ArrayDeque<Item> {
             resizeFlag = true;
         }
         items[nextLast] = x;
-        if (nextLast != size - 1) {
+        if (nextLast != length - 1) {
             nextLast++;
         } else {
             nextLast = 0;
         }
         if (resizeFlag) {
-            resizeBig(2 * size);
+            resizeBig(2 * length);
         }
     }
 
-    public void removeFirst() {
-        if (nextFirst == size) {
+    public Item removeFirst() {
+        if(size() <= 0) {
+            return null;
+        }
+        Item temp = null;
+        if (nextFirst == length - 1) {
             nextFirst = 0;
         } else {
             nextFirst++;
         }
+        temp = items[nextFirst];
         items[nextFirst] = null;
-        if(size >= 16) {
-            if(size > 4 * (nextLast - nextFirst) && nextLast > nextFirst) {
-                resizeSmall(size / 2);
+        if(length >= 16) {
+            if(length > 4 * (nextLast - nextFirst) && nextLast > nextFirst) {
+                resizeSmall(length / 2);
             }
         }
+        return temp;
     }
 
-    public void removeLast() {
-        if (nextLast == 0) {
-            nextFirst = size;
-        } else {
-            nextFirst--;
+    public Item removeLast() {
+        if(size() <= 0) {
+            return null;
         }
+        Item temp = null;
+        if (nextLast == 0) {
+            nextLast = length - 1;
+        } else {
+            nextLast--;
+        }
+        temp = items[nextLast];
         items[nextLast] = null;
-        if(size >= 16) {
-            if(size > 4 * (nextLast - nextFirst) && nextLast > nextFirst) {
-                resizeSmall(size / 2);
+        if(length >= 16) {
+            if(length > 4 * (nextLast - nextFirst) && nextLast > nextFirst) {
+                resizeSmall(length / 2);
             }
         }
+        return temp;
     }
 
     public Item get(int index) {
         int res = 0;
         res = index + nextFirst;
-        if (res > size) {
-            return items[res - size + 1];
+        if (res > length) {
+            return items[res - length + 1];
         }
         return items[res + 1];
     }
 
     public int size() {
-        return size;
+        if (nextFirst >= nextLast) {
+            return length - (nextFirst - nextLast) - 1;
+        }
+        return nextLast - nextFirst - 1;
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return size() == 0;
+    }
+
+    public void printDeque() {
+        int temp = nextFirst + 1;
+        /*if (nextFirst == length - 1) {
+            temp = 0;
+        } else {
+            temp = nextFirst + 1;
+        }*/
+        while(temp != nextLast - 1) {
+            if (temp == length) {
+                temp = 0;
+            }
+            System.out.print(items[temp]);
+            temp++;
+        }
+        System.out.println(items[temp]);
     }
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -1,2 +1,51 @@
-package deque;public class ArrayDequeTest {
+package deque;
+
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+
+public class ArrayDequeTest {
+    @Test
+    public void RandomizedTest_1() {
+
+        ArrayDeque<Integer> correct = new ArrayDeque<>();
+        //BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 50000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                //broken.addLast(randVal);
+                //System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1 && correct.size() > 0) {
+                // getLast
+                int last = correct.removeLast();
+                //int last1 = broken.getLast();
+                //System.out.println("Last: " + last);
+                //System.out.println("Last1: " + last1);
+            } else if (operationNumber == 2) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addFirst(randVal);
+                //int size1 = broken.size();
+                //System.out.println("size: " + size);
+                //System.out.println("size1: " + size1);
+            } else if (operationNumber == 3 && correct.size() > 0) {
+                // getLast
+                int first = correct.removeFirst();
+                //int last1 = broken.getLast();
+                //System.out.println("Last: " + last);
+                //System.out.println("Last1: " + last1);
+            } else if (operationNumber == 4 && correct.size() > 0 ) {
+                // size
+                int size = correct.size();
+                // removeLast
+                //correct.removeLast();
+                //broken.removeLast();
+                //System.out.println("removeLast");
+            }
+        }
+    }
 }
